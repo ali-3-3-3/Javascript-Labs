db0ad0ace2e01c8fb8e190c54c04ddff181459a5
ali-3-3-3 <aliya.mohdwari@gmail.com>
2025-03-10 22:06:32 +0800

    Authentication

--- index.js
+++ index.js
@@ -1,46 +1,85 @@
-//NodeJS uses the CommonJS module system (i.e. the "require()") 
-//as opposed to ES6 modules (similar concept) but different syntax 
-const express = require("express"); 
-const app = express(); 
-const port = 3000; 
+//NodeJS uses the CommonJS module system (i.e. the "require()")
+//as opposed to ES6 modules (similar concept) but different syntax
+const express = require("express");
+const app = express();
+const port = 3000;
 app.set("view engine", "ejs");
-const { insertCustomer, getAllCustomers, deleteCustomer, getCustomerById, updateCustomer } = require("./lib/database"); 
+const {
+  insertCustomer,
+  getAllCustomers,
+  deleteCustomer,
+  getCustomerById,
+  updateCustomer,
+} = require("./lib/database");
 
 app.use(express.static("public"));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 
-app.get("/", (req, res) => { 
-    res.render("main");
-  }); 
+const session = require("express-session");
+app.use(
+  session({
+    secret: "SOME_SECRET_KEY",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false, //setting this false for http connections
+    },
+  })
+);
 
-app.get("/customers/new", (req, res) => {
-    res.render("customer_form");
+function requireAuth(req, res, next) {
+  if (req.session && req.session.userId) {
+    next(); //continue - pass to the next middleware
+  } else {
+    //bounce the user to the login page
+    res.redirect("/login");
+  }
+}
+
+app.get("/", (req, res) => {
+  res.render("main");
 });
 
-app.post("/customers/add", async (req, res) => {
-    let { name, gender, birthday } = req.body;
+app.get("/customers/new", requireAuth, (req, res) => {
+  res.render("customer_form");
+});
 
-    birthday = new Date(birthday);
+app.post("/customers/add", requireAuth, async (req, res) => {
+  let { name, gender, birthday } = req.body;
 
-    dateCreated = new Date();
+  birthday = new Date(birthday);
 
-    const customer = await insertCustomer({ name, gender, birthday, created: dateCreated});
-  
-    console.log("Form Data Received:", customer);
-  
-    res.redirect("/customers/list");
-  });
-  
+  dateCreated = new Date();
 
-app.get("/customers/list", async (req, res) => {
+  const customer = await insertCustomer(
+    {
+      name,
+      gender,
+      birthday,
+      created: dateCreated,
+    },
+    req.session.userId
+  );
+
+  console.log("Form Data Received:", customer);
+
+  res.redirect("/customers/list");
+});
+
+app.get("/customers/list", requireAuth, async (req, res) => {
   try {
-    let customers = await getAllCustomers();
-    customers = customers.map(customer => ({
+    let customers = await getAllCustomers(req.session.userId);
+    customers = customers.map((customer) => ({
       ...customer,
-      gender: customer.gender == 1 ? "Female" : customer.gender == 2 ? "Male" : "Unknown"
+      gender:
+        customer.gender == 1
+          ? "Female"
+          : customer.gender == 2
+          ? "Male"
+          : "Unknown",
     }));
     res.render("list_customers", { customers });
   } catch (e) {
@@ -49,42 +88,63 @@ app.get("/customers/list", async (req, res) => {
   }
 });
 
-app.post("/customers/:id/delete", async (req, res) => {
+app.post("/customers/:id/delete", requireAuth, async (req, res) => {
   try {
-      const { id } = req.params;
-      await deleteCustomer(id);
-      res.redirect("/customers/list"); // Refresh the list after deleting
+    const { id } = req.params;
+    await deleteCustomer(id);
+    res.redirect("/customers/list"); // Refresh the list after deleting
   } catch (error) {
-      console.error("Error deleting customer:", error);
-      res.status(500).send("Failed to delete customer");
+    console.error("Error deleting customer:", error);
+    res.status(500).send("Failed to delete customer");
   }
 });
 
-app.get("/customers/:id", async (req, res) => {
+app.get("/customers/:id", requireAuth, async (req, res) => {
   const customerId = req.params.id;
 
   try {
-      const customer = await getCustomerById(customerId);
-      console.log("Customer:", customer); // Debugging
-      res.render("customer_form", { 
-        ...customer }); // Render the edit form
+    const customer = await getCustomerById(customerId);
+    console.log("Customer:", customer); // Debugging
+    res.render("customer_form", {
+      ...customer,
+    }); // Render the edit form
   } catch (error) {
-      console.error("Error fetching customer:", error);
-      res.status(500).send("Error retrieving customer");
-  }
-});
-  
-app.post("/customers/:id/edit", async (req, res) => {
-  console.log("Received body:", req.body); // Debugging
-  try {
-      await updateCustomer(req.params.id, req.body);
-      res.redirect("/customers/list"); // Redirect to customer list
-  } catch (error) {
-      console.error("Error updating customer:", error);
-      res.status(500).send("Failed to update customer");
+    console.error("Error fetching customer:", error);
+    res.status(500).send("Error retrieving customer");
   }
 });
 
-app.listen(port, () => { 
-  console.log(`Example app listening at http://localhost:${port}`); 
+app.post("/customers/:id/edit", requireAuth, async (req, res) => {
+  console.log("Received body:", req.body); // Debugging
+  try {
+    await updateCustomer(req.params.id, req.body);
+    res.redirect("/customers/list"); // Redirect to customer list
+  } catch (error) {
+    console.error("Error updating customer:", error);
+    res.status(500).send("Failed to update customer");
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/dologin", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await getUserByUsername(username);
+  if (foundUser && foundUser.password === password) {
+    req.session.userId = foundUser._id;
+    res.redirect("/customers/list");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.session.userId = null;
+  res.redirect("/login");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
